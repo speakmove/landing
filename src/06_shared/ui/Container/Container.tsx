cd7f6b513823e578a4327b5caa0e3bdf948abb02
DmitriyOne <senkoner4@gmail.com>
2026-05-13 19:07:14 +0100

    feat(shared/ui): add Container with responsive max-width and padding

---
+++ src/06_shared/ui/Container/Container.tsx
@@ -0,0 +1,10 @@
+import type { PropsWithChildren } from 'react';
+import { cn } from '@/shared/model/libs/cn';
+
+type TProps = PropsWithChildren<{
+  className?: string;
+}>;
+
+export function Container({ children, className }: TProps) {
+  return <div className={cn('mx-auto w-full max-w-[1180px] px-5', className)}>{children}</div>;
+}
