c1c36d928ddb95fb57411c4fda683f0f3db1620b
DmitriyOne <senkoner4@gmail.com>
2026-05-13 13:59:09 +0100

    feat(shared): add env config with @t3-oss/env-nextjs

---
+++ src/06_shared/model/libs/env/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  },
+  client: {
+    NEXT_PUBLIC_SITE_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  emptyStringAsUndefined: true,
+});
